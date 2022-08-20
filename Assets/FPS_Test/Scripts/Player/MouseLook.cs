3861e746e3df5f8f1ed9d8497b610fea20afac87
Sk3letor <jsaarelainen@hotmail.fi>
2022-08-20 06:30:56 +0300

    Basic Firstperson
    -Movement
    -Run ( Shift button )
    - Jump ( Space button )
    - Mouselook

---
+++ Assets/FPS_Test/Scripts/Player/MouseLook.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private float cameraheight = 1.4f;
+
+    [SerializeField] private float mouseSensitivity = 8f;
+
+    private PlayerInput playerInput;
+    private PlayerInputActions playerInputActions;
+
+    [SerializeField] bool canTurn = true;
+    private float mouseX, mouseY;
+    private float xRotation;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+    }
+
+    private void Start()
+    {
+        if (mainCamera == null)
+        {
+            mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        }
+
+        mainCamera.transform.position = transform.position + new Vector3(0, cameraheight, 0);
+        mainCamera.transform.parent = transform;
+
+        xRotation = 0;
+    }
+
+    private void Update()
+    {
+        if (canTurn)
+        {
+            RotateCamera();
+            RotatePlayer();
+        }
+    }
+
+    void RotatePlayer()
+    {
+        mouseX = playerInputActions.Player.MouseX.ReadValue<float>() * mouseSensitivity * Time.deltaTime;
+        mouseY = playerInputActions.Player.MouseY.ReadValue<float>() * mouseSensitivity * Time.deltaTime;
+
+        transform.Rotate(Vector3.up * mouseX);
+    }
+
+    void RotateCamera()
+    {
+        xRotation -= mouseY;
+
+        // Limit mouse
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        mainCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+}
---
+++ Assets/FPS_Test/Scripts/Player/firstPersonControl.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+public class firstPersonControl : MonoBehaviour
+{
+    private CharacterController cc;
+    private PlayerInput playerInput;
+    private PlayerInputActions playerInputActions;
+
+    [Header("Player's Features")]
+    [SerializeField] private bool canMove = true;
+    [SerializeField] private float walkingSpeed = 3.2f;
+    [SerializeField] private float runningSpeed = 4.6f;
+    [Space]
+    [SerializeField] private float jumpingForce = 1.4f;
+    [SerializeField] private bool canJump = true;
+
+    private float inputX, inputZ;
+    private float movingSpeed;
+    private Vector3 moveDirection;
+    private Vector3 velocity;
+
+    //Crouch variables
+    private float orginalHeight;
+    private float wantedHeight;
+
+    private void Awake()
+    {
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+        playerInputActions.Player.Jump.performed += Jump;
+    }
+
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        //mouseLook = gameObject.GetComponent<MouseLook>();
+        cc = gameObject.GetComponent<CharacterController>();
+        playerInput = GetComponent<PlayerInput>();
+
+        cc.center = new Vector3(0, 1, 0);
+        cc.height = 2;
+        cc.radius = 0.3f;
+        cc.stepOffset = 0.3f;
+
+        //movingSpeed = walkSpeed;
+
+        orginalHeight = cc.height;
+        wantedHeight = orginalHeight / 2;
+    }
+
+    void Update()
+    {
+        if (canMove)
+        {
+            Inputs();
+        }
+
+        AdjustSpeed();
+        Move();
+    }
+
+    void Inputs()
+    {
+        inputX = playerInputActions.Player.Movement.ReadValue<Vector2>().x;
+        inputZ = playerInputActions.Player.Movement.ReadValue<Vector2>().y;
+    }
+
+    void AdjustSpeed()
+    {
+       if(playerInputActions.Player.Run.IsPressed() && movingSpeed != runningSpeed)
+        {
+            movingSpeed = runningSpeed;
+        }
+
+       else
+        {
+            movingSpeed = walkingSpeed;
+        }
+    }
+
+    void Move()
+    {
+        moveDirection = (transform.right * inputX) + (transform.forward * inputZ);
+        cc.Move(moveDirection.normalized * movingSpeed * Time.deltaTime);
+
+        velocity.y -= 9.81f * Time.deltaTime; //Gravity
+        cc.Move(velocity * Time.deltaTime);
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && canJump)
+        {
+            if(isGrounded) velocity.y = Mathf.Sqrt(jumpingForce * -2f * Physics.gravity.y);
+        }
+        //isJumping = true;
+        //coolDownJumping = 0.2f;
+    }
+
+    private bool isGrounded
+    {
+        get
+        {
+            return
+                Physics.Raycast(transform.position + new Vector3(0, 0.25f, 0), Vector3.down, 0.5f);
+        }
+    }
+}
